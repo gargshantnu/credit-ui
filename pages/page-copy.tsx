8a5db45003fb02b1be2eed92baef9e1b1dc9e02e
Shantnu Garg <gargshantnu@gmail.com>
2023-07-11 20:16:27 +0530

    Add files via upload

---
+++ pages/page-copy.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home2() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        Shantnu
+      </div>
+    </main>
+  )
+}

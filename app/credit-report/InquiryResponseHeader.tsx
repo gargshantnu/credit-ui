7c155d1fded30d0038518d6f6703092480f97011
Shantnu Garg <gargshantnu@gmail.com>
2023-07-12 19:43:08 +0530

    beautify header section

--- app/credit-report/InquiryResponseHeader.tsx
+++ app/credit-report/InquiryResponseHeader.tsx
@@ -1,15 +1,45 @@
 import styles from './styles.module.css'
 
 
-export default function InquiryResponseHeader(data: Object) {
+interface InquiryResponseHeaderData {
+    "ClientID": string,
+    "CustRefField": string,
+    "ReportOrderNO": string,
+    "ProductCode": string[],
+    "SuccessCode": string,
+    "Date": string,
+    "Time": string
+}
+
+
+
+export default function InquiryResponseHeader(data: InquiryResponseHeaderData) {
     return (
         <>
             <div className={styles.Heading}>
                 InquiryResponseHeader
             </div>
-            <div>
-                {JSON.stringify(data)}
+            <div className={styles.Data}>
+                <span>
+                    Your client id is: {data.ClientID}
+                </span>
+                <span>
+                    Your Customer Ref Field is: {data.CustRefField}
+                </span>
+                <span>
+                    Your report order number is: {data.ReportOrderNO}
+                </span>
+                <span>
+                    Your product code is: {data.ProductCode.join(", ")}
+                </span>
+                <span>
+                    Your success code is: {data.SuccessCode}
+                </span>
+                <span>
+                    Report generation date: {data.Date} {data.Time}
+                </span>
             </div>
+            
         </>
     )
 }
